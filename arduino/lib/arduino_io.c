07a6ebb2b7eae872010fa1ed0267b97aade8bdef
charlesvdv <charles.vandevoorde@hotmail.be>
2016-07-10 11:54:08 +0200

    arduino_io: more resiliant to external mistakes

--- arduino/lib/arduino_io.c
+++ arduino/lib/arduino_io.c
@@ -1,6 +1,11 @@
 #include "../register/reg_pin.h"
+#include "arduino_io.h"
 
 void arduino_lib_set_io_mode(int pin_number, int mode) {
+    if (mode != OUTPUT || mode != INPUT) {
+        // ERROR: warn the user and stop the program
+        return;
+    }
     reg_set_pin_mode(pin_number, mode);
 }
 
@@ -9,11 +14,18 @@ int arduino_lib_get_io_value(int pin_number) {
     if (ret == PIN_NOT_DEFINED_ERROR) {
         // ERROR: warn the user and stop the program
         return -1;
+    } else if (ret != LOW || ret != HIGH) {
+        // ERROR: warn the user and stop the program
+        return -1;
     }
     return ret;
 }
 
 void arduino_lib_set_io_value(int pin_number, int value) {
+    if (value != LOW || value != HIGH) {
+        // ERROR: warn the user and stop the program
+        return;
+    }
     int val = reg_set_pin_value(pin_number, value);
     if (val == PIN_NOT_DEFINED_ERROR) {
         // ERROR: warn the user and stop the program
