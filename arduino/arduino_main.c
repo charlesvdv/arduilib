13d3226596a20dc0074eac9a62dbf7443eef6545
charlesvdv <charles.vandevoorde@hotmail.be>
2016-08-13 17:16:57 +0200

    arduino_main: save state after the loop finish

--- arduino/arduino_main.c
+++ arduino/arduino_main.c
@@ -13,6 +13,9 @@ int main() {
     setup();
     for(; arduilib_get_time_micros() < MAX_TIME_RUNNING; reg_increment_time()) {
         loop();
+        PinState* states = reg_dump_pin_data();
+        arduilib_log_io(arduilib_get_time_micros(), states, PIN_NUMBER);
+        free(states);
     }
     char *s = arduilib_log_dump();
     printf("%s\n", s);
