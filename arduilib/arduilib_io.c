a3071e6e5e4e2312db26f7156296691bb4956c86
charlesvdv <charles.vandevoorde@hotmail.be>
2016-08-25 20:59:22 +0200

    switch time handling from register to arduilib
    which make more sense because all loggings are now in milliseconds and
    register should only be used by the arduilib specifics methods.

--- arduilib/arduilib_io.c
+++ arduilib/arduilib_io.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 #include "../register/reg_pin.h"
-#include "../register/reg_time.h"
 
 #include "arduilib_io.h"
 #include "arduilib_error.h"
+#include "arduilib_time.h"
 
 #define ERR_MSG_SIZE 200
 
@@ -19,7 +19,7 @@ void arduilib_set_io_mode(int pin_number, int mode) {
     if (mode != OUTPUT && mode != INPUT) {
         snprintf(err_msg, ERR_MSG_SIZE, ERR_WRONG_MODE,
                 pin_number, OUTPUT, INPUT, mode);
-        arduilib_error_throw(reg_get_time(), err_msg, ERR_CODE_WRONG_MODE);
+        arduilib_error_throw(arduilib_get_time_millis(), err_msg, ERR_CODE_WRONG_MODE);
     }
     reg_set_pin_mode(pin_number, mode);
 }
@@ -28,7 +28,7 @@ int arduilib_get_io_mode(int pin_number) {
     int mode = reg_get_pin_mode(pin_number);
     if (mode == PIN_NOT_DEFINED_ERROR) {
         snprintf(err_msg, ERR_MSG_SIZE, ERR_PIN_UNDEFINED, pin_number);
-        arduilib_error_throw(reg_get_time(), err_msg, ERR_CODE_PIN_UNDEFINED);
+        arduilib_error_throw(arduilib_get_time_millis(), err_msg, ERR_CODE_PIN_UNDEFINED);
     }
     return mode;
 }
@@ -37,11 +37,11 @@ int arduilib_get_io_value(int pin_number) {
     int ret = reg_get_pin_value(pin_number);
     if (ret == PIN_NOT_DEFINED_ERROR) {
         snprintf(err_msg, ERR_MSG_SIZE, ERR_PIN_UNDEFINED, pin_number);
-        arduilib_error_throw(reg_get_time(), err_msg, ERR_CODE_PIN_UNDEFINED);
+        arduilib_error_throw(arduilib_get_time_millis(), err_msg, ERR_CODE_PIN_UNDEFINED);
     } else if (ret != LOW && ret != HIGH) {
         snprintf(err_msg, ERR_MSG_SIZE, ERR_WRONG_VALUE,
                 pin_number, LOW, HIGH, ret);
-        arduilib_error_throw(reg_get_time(), err_msg, ERR_CODE_WRONG_VALUE);
+        arduilib_error_throw(arduilib_get_time_millis(), err_msg, ERR_CODE_WRONG_VALUE);
     }
     return ret;
 }
@@ -50,15 +50,15 @@ void arduilib_set_io_value(int pin_number, int value) {
     if (value != LOW && value != HIGH) {
         snprintf(err_msg, ERR_MSG_SIZE, ERR_WRONG_VALUE,
                 pin_number, OUTPUT, INPUT, value);
-        arduilib_error_throw(reg_get_time(), err_msg, ERR_CODE_WRONG_VALUE);
+        arduilib_error_throw(arduilib_get_time_millis(), err_msg, ERR_CODE_WRONG_VALUE);
     }
     int val = reg_set_pin_value(pin_number, value);
     if (val == PIN_NOT_DEFINED_ERROR) {
         snprintf(err_msg, ERR_MSG_SIZE, ERR_PIN_UNDEFINED, pin_number);
-        arduilib_error_throw(reg_get_time(), err_msg, ERR_CODE_PIN_UNDEFINED);
+        arduilib_error_throw(arduilib_get_time_millis(), err_msg, ERR_CODE_PIN_UNDEFINED);
     } else if (val == WRONG_MODE_ERROR) {
         snprintf(err_msg, ERR_MSG_SIZE, ERR_WRONG_MODE_DEFINED,
                 pin_number);
-        arduilib_error_throw(reg_get_time(), err_msg, ERR_CODE_WRONG_MODE);
+        arduilib_error_throw(arduilib_get_time_millis(), err_msg, ERR_CODE_WRONG_MODE);
     }
 }
